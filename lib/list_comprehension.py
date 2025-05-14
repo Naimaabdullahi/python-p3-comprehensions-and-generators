bc337d1f2e87f25352aa5dab036b1db996a70c0a
Naimaabdullahi <naima.noor@student.moringaschool.com>
2025-05-14 16:40:49 +0300

    commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even = []
+    for n in num_list:
+        if n % 2 == 0:
+            even.append(n)
+            print(even)
+    return even
 
 def make_exclamation(sentence_list):
-    pass
+    result = []
+    for sentence in sentence_list:
+        result.append(sentence + "!")
+    return(result)
